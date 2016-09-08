681c78b8cf2fbf94c8a662f8f6cc72785b191f88
Matin Massoudi <matin@massoudi.net>
2016-09-08 11:33:12 -0700

    Updated Mho class
    Added getters, setters,

--- Mho.java
+++ Mho.java
@@ -1,13 +1,48 @@
+import java.awt.Canvas;
 
-public class Mho {
-	//Default constructor
-	public Mho(){
-		
-	}
+public class Mho{
+	//Private data fields
+	private int x;
+	private int y;
 	
 	//Main constructor
-	public Mho(int x, int y){
-		
+	public Mho(){
+		x = randRange();
+		y = randRange();
+		boolean loopVar = true;
+		while(loopVar){
+			if(x == 0  x  ){
+				
+			}
+		}
 	}
+	
+	/**
+	 * Randomize location for the Mhos
+	 * @return
+	 */
+	public int randRange(){
+		return 12+(int)(Math.random()*12);
+	}
+	
+	
+	//Getters and setters
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
 	
 }
