4c2a46d40d50bcdd30d1792b5b66e019ee110e5b
IvayloKozhuharov <ivayloviii@gmail.com>
2016-09-13 10:22:18 -0700

    New update, file reading and button reading

--- HiVoltsMain.java
+++ HiVoltsMain.java
@@ -1,11 +1,20 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Iterator;
 
 import javax.swing.JFrame;
 
-public class HiVoltsMain extends Canvas {
+public class HiVoltsMain extends Canvas implements KeyListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		Canvas canvas = new HiVoltsMain();
@@ -13,6 +22,16 @@ public class HiVoltsMain extends Canvas {
 		frame.getContentPane().add(canvas);
 		frame.pack();
 		frame.setVisible(true);
+		
+		
+		File gameField = new File("C:\\Users\\Ivo\\workspace\\Hivolts\\game-field.txt");
+		Iterator<String> fieldLinesIt = Files.lines(gameField.toPath()).iterator();
+		while (fieldLinesIt.hasNext()) {
+			String fieldLine = fieldLinesIt.next();
+			// initialize game matrix
+			System.out.println(fieldLine);
+		}
+//		[][] Matrix; 
 	}
 	
 
@@ -28,11 +47,43 @@ public class HiVoltsMain extends Canvas {
 	}
 	
 
+	int keyBla;
 	/** Paint Method
 	 * @param g - object of the graphics package
 	 */
 	public void paint(Graphics g) {
-	
+		this.addKeyListener(this);
+		System.out.println(keyBla);
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		repaint();
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		keyBla=arg0.getKeyCode();
+//		System.out.println("keyPressed:" + arg0.getKeyCode());
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		keyBla=0;
+//		System.out.println("keyReleased:" + e.toString());
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+//		System.out.println("keyTyped:" + e.toString());
 	}
 	
 }
