5c744e696ed30eb1d137617d5b9a6f8f1a953924
IvayloKozhuharov <ivayloviii@gmail.com>
2016-09-15 11:18:55 -0700

    RNGesus has selected the positions of your enemies.

--- HiVoltsMain.java
+++ HiVoltsMain.java
@@ -4,7 +4,9 @@ import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Objects;
 
 import javax.swing.JFrame;
 
@@ -13,6 +15,21 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public static ArrayList<int[]> freeSpacefinder(ArrayList<int[]> freeerSpaces){
+		for(int br=1;br<=10;br++){
+			for(int br1=1;br1<=10;br1++){
+				int[] curentPos={br,br1};
+				freeerSpaces.add(curentPos);
+//				System.out.println(freeerSpaces.get((br-1)*10+(br1-1))[0]);
+//				System.out.println(freeerSpaces.get((br-1)*10+(br1-1))[1]);
+			}
+		}
+		return freeerSpaces;
+	}
+	public static int RNGesus(int randsize){
+		return (int)(1+Math.random()*randsize);
+	}
 	
 	public static void main(String[] args) throws Exception {
 		JFrame frame = new JFrame();
@@ -23,18 +40,40 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 		frame.pack();
 		frame.setVisible(true);
 		
-		File save = new File("game-field.txt");
-		String path = save.getAbsolutePath();
 		
-	//	File gameField = new File("C:\\Users\\Ivo\\workspace\\Hivolts\\game-field.txt");
-		File gameField = new File(path);
+		File gameField = new File("C:\\Users\\Ivo\\workspace\\Hivolts\\game-field.txt");
 		Iterator<String> fieldLinesIt = Files.lines(gameField.toPath()).iterator();
 		while (fieldLinesIt.hasNext()) {
 			String fieldLine = fieldLinesIt.next();
 			// initialize game matrix
 			System.out.println(fieldLine);
 		}
-//		[][] Matrix; 
+		Obstacles[][] Matrix = new Obstacles[12][12];
+//		ArrayList[][] freeSpaces= new ArrayList[10][10];
+		ArrayList<int[]> freeerSpaces= new ArrayList<int[]>();
+		freeerSpaces=freeSpacefinder(freeerSpaces);
+		int[][]MhoPositions=new int[12][2];
+		int[][]FencePositions=new int[20][2];
+		for(int br=0;br<12;br++){
+			int RNGHelper=RNGesus(freeerSpaces.size());
+			MhoPositions[br][0]=freeerSpaces.get(RNGHelper)[0];
+			MhoPositions[br][1]=freeerSpaces.get(RNGHelper)[1];
+			freeerSpaces.remove(RNGHelper);
+		}
+		for(int br=0;br<20;br++){
+			int RNGHelper=RNGesus(freeerSpaces.size());
+			FencePositions[br][0]=freeerSpaces.get(RNGHelper)[0];
+			FencePositions[br][1]=freeerSpaces.get(RNGHelper)[1];
+			freeerSpaces.remove(RNGHelper);
+		}
+		for(int br=0;br<12;br++){
+			System.out.println(MhoPositions[br][0]+", "+MhoPositions[br][1]);
+		}
+		System.out.println();
+		for(int br=0;br<20;br++){
+			System.out.println(FencePositions[br][0]+", "+FencePositions[br][1]);
+		}
+
 	}
 	
 
@@ -56,7 +95,7 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 	 */
 	public void paint(Graphics g) {
 		this.addKeyListener(this);
-		System.out.println(keyBla);
+//		System.out.println(keyBla);
 		try {
 			Thread.sleep(100);
 		} catch (InterruptedException e) {
