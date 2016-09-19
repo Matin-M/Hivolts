471a0efe56e6f61f3ce54f41f497911eaef278cc
Matin Massoudi <matin@massoudi.net>
2016-09-19 11:11:29 -0700

    Updated Fence Class

--- Fence.java
+++ Fence.java
@@ -1,13 +1,74 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class Fence {
-	//Default constructor
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Fence extends Obstacles{
+	//Private data fields
+	private int x;
+	private int y;
+	
 	public Fence(){
 		
 	}
-	
 	//Main constructor
-	public Fence(int x, int y){
+	//Constructor formal parameters may vary depending on specifications. 
+	public Fence(int width, int height){
 		
 	}
+	/**
+	 * paint Fences individualy when called 
+	 * @param g - Object of Graphics package
+	 */
+	
+	public BufferedImage imageMaker(){
+		//Name of the file in workspace
+		File image = new File("California.PNG");
+		String path = image.getAbsolutePath();
+		BufferedImage img = null;
+		
+		 try {
+             img = ImageIO.read(new File(path));
+         } catch (IOException e) {
+        	 System.out.println("Oops! There was an error: " + e);
+         }
+		 return img; 
+	}
+	
+	/**
+	 * Randomize location for the Fences
+	 * @return
+	 */
+	/*
+	public int randRange(){
+		return 12+(int)(Math.random()*12);
+	}
+	*/
+	
+	//Getters and setters
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
 	
 }
