f38e535f1a676540310ed3e969767d6efbe0485e
Matin Massoudi <matin@massoudi.net>
2016-09-20 10:02:58 -0700

    Painting methods

--- HiVoltsMain.java
+++ HiVoltsMain.java
@@ -32,22 +32,34 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 	}
 	
 	public static void main(String[] args) throws Exception {
+		final int xRes = 600;
+		final int yRes = 600;
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		Canvas canvas = new HiVoltsMain();
-		canvas.setSize(1000,1900);
+		canvas.setSize(xRes, yRes);
 		frame.getContentPane().add(canvas);
+		frame.setPreferredSize(new Dimension(xRes, yRes));
+		frame.setSize(xRes, yRes);
 		frame.pack();
 		frame.setVisible(true);
 		
 		
-		File gameField = new File("C:\\Users\\Ivo\\workspace\\Hivolts\\game-field.txt");
-		Iterator<String> fieldLinesIt = Files.lines(gameField.toPath()).iterator();
-		while (fieldLinesIt.hasNext()) {
-			String fieldLine = fieldLinesIt.next();
-			// initialize game matrix
-			System.out.println(fieldLine);
+		//Redo try catch
+		try{
+			File save = new File("game-field.txt");
+			String path = save.getAbsolutePath();
+			File gameField = new File(path);
+			Iterator<String> fieldLinesIt = Files.lines(gameField.toPath()).iterator();
+			while (fieldLinesIt.hasNext()) {
+				String fieldLine = fieldLinesIt.next();
+				// initialize game matrix
+				System.out.println(fieldLine);
+			}
+		}catch(Exception e){
+			System.err.println("The game file was not located! Specific Error: " + e.getMessage());
 		}
+		
 		Obstacles[][] Matrix = new Obstacles[12][12];
 //		ArrayList[][] freeSpaces= new ArrayList[10][10];
 		ArrayList<int[]> freeerSpaces= new ArrayList<int[]>();
@@ -76,19 +88,68 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 			System.out.println(FencePositions[br][0]+", "+FencePositions[br][1]);
 			Matrix[FencePositions[br][0]][FencePositions[br][1]]=new Fence();
 		}
+		for(int i = 0; i < 12; i++){
+			for(int x = 0; x < 12; x++){
+				if(Matrix[i][x] instanceof Fence){
+					
+				}else if(){
+					
+				}else if(){
+					
+				}
+			}
+		}
 		
 	}
 	
-
+	public void 
+	
 	public HiVoltsMain() {
 		init();	
 	}
 	
 	
 	public void init() {
-		setSize(1900,1000);
 		setBackground(Color.white);
 		repaint();
+	}
+	
+	
+	
+	//Test Mhos
+	public void pMho(Graphics g){
+		Mho test = new Mho();
+		System.out.println(getWidth()+" "+getHeight());
+		g.drawImage(test.imageMaker(), 162, 162, getWidth()/12, getHeight()/12, null);
+	}
+	
+	public void outerFence(Graphics g){
+		Fence outer = new Fence();
+		int lineX = getWidth()/12;
+		int lineY = getHeight()/12;
+		int xmod = 0;
+		for(int i = 1; i <= 12; i++){
+			g.drawImage(outer.imageMaker(), xmod, 0, getWidth()/12, getHeight()/12, null);
+			xmod += lineX; 
+		}
+		
+	}
+	
+	public void paintGrid(Graphics g){
+		//Paint a grid
+		int lineX = getWidth()/12;
+		int lineY = getHeight()/12;
+		int xmod = lineX;
+		for(int i = 1; i <= 12; i++){
+			g.drawLine(xmod, 0, xmod, getHeight());
+			xmod += lineX; 
+		}
+
+		int ymod = lineY;
+		for(int i = 1; i <= 12; i++){
+			g.drawLine(0, ymod, getWidth(), ymod);
+			ymod += lineY; 
+		}
 	}
 	
 
@@ -97,6 +158,10 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 	 * @param g - object of the graphics package
 	 */
 	public void paint(Graphics g) {
+		pMho(g);
+		outerFence(g);
+		paintGrid(g);
+		
 		this.addKeyListener(this);
 //		System.out.println(keyBla);
 		try {
