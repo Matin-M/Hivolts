bcd6c7033355ba20507a872b5c89df46c1664a4c
Matin Massoudi <matin@massoudi.net>
2016-09-20 10:16:25 -0700

    Hivolts main comparison class

--- HiVoltsMain.java
+++ HiVoltsMain.java
@@ -1,13 +1,13 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Objects;
 
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class HiVoltsMain extends Canvas implements KeyListener {
@@ -16,29 +16,99 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public static ArrayList<int[]> freeSpacefinder(ArrayList<int[]> freeerSpaces){
+		for(int br=1;br<=10;br++){
+			for(int br1=1;br1<=10;br1++){
+				int[] curentPos={br,br1};
+				freeerSpaces.add(curentPos);
+//				System.out.println(freeerSpaces.get((br-1)*10+(br1-1))[0]);
+//				System.out.println(freeerSpaces.get((br-1)*10+(br1-1))[1]);
+			}
+		}
+		return freeerSpaces;
+	}
+	public static int RNGesus(int randsize){
+		return (int)(1+Math.random()*randsize);
+	}
+	
 	public static void main(String[] args) throws Exception {
+		final int xRes = 600;
+		final int yRes = 600;
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		Canvas canvas = new HiVoltsMain();
-		canvas.setSize(1000,1000);
+		canvas.setSize(xRes, yRes);
 		frame.getContentPane().add(canvas);
+		frame.setPreferredSize(new Dimension(xRes, yRes));
+		frame.setSize(xRes, yRes);
 		frame.pack();
 		frame.setVisible(true);
 		
-		File save = new File("game-field.txt");
-		String path = save.getAbsolutePath();
-	//	File gameField = new File("C:\\Users\\Ivo\\workspace\\Hivolts\\game-field.txt");
-		File gameField = new File(path);
-		Iterator<String> fieldLinesIt = Files.lines(gameField.toPath()).iterator();
-		while (fieldLinesIt.hasNext()) {
-			String fieldLine = fieldLinesIt.next();
-			// initialize game matrix
-			System.out.println(fieldLine);
+		
+		//Redo try catch
+		File save = null;
+		String path = null;
+		try{
+			save = new File("game-field.txt");
+			path = save.getAbsolutePath();
+		}catch(Exception e){
+			System.err.println("The game file was not located! Specific Error: " + e.getMessage());
 		}
-//		[][] Matrix; 
+			File gameField = new File(path);
+			Iterator<String> fieldLinesIt = Files.lines(gameField.toPath()).iterator();
+			while (fieldLinesIt.hasNext()) {
+				String fieldLine = fieldLinesIt.next();
+				// initialize game matrix
+				System.out.println(fieldLine);
+			}
+		
+		
+		Obstacles[][] Matrix = new Obstacles[12][12];
+//		ArrayList[][] freeSpaces= new ArrayList[10][10];
+		ArrayList<int[]> freeerSpaces= new ArrayList<int[]>();
+		freeerSpaces=freeSpacefinder(freeerSpaces);
+		int[][]MhoPositions=new int[12][2];
+		int[][]FencePositions=new int[20][2];
+		for(int br=0;br<12;br++){
+			int RNGHelper=RNGesus(freeerSpaces.size());
+			MhoPositions[br][0]=freeerSpaces.get(RNGHelper)[0];
+			MhoPositions[br][1]=freeerSpaces.get(RNGHelper)[1];
+			freeerSpaces.remove(RNGHelper);
+		}
+		for(int br=0;br<20;br++){
+			int RNGHelper=RNGesus(freeerSpaces.size());
+			FencePositions[br][0]=freeerSpaces.get(RNGHelper)[0];
+			FencePositions[br][1]=freeerSpaces.get(RNGHelper)[1];
+			freeerSpaces.remove(RNGHelper);
+		}
+		
+		for(int br=0;br<12;br++){
+			System.out.println(MhoPositions[br][0]+", "+MhoPositions[br][1]);
+			Matrix[MhoPositions[br][0]][MhoPositions[br][1]]=new Mho();
+		}
+		System.out.println();
+		for(int br=0;br<20;br++){
+			System.out.println(FencePositions[br][0]+", "+FencePositions[br][1]);
+			Matrix[FencePositions[br][0]][FencePositions[br][1]]=new Fence();
+		}
+		/*
+		for(int i = 0; i < 12; i++){
+			for(int x = 0; x < 12; x++){
+				if(Matrix[i][x] instanceof Fence){
+					
+				}else if(){
+					
+				}else if(){
+					
+				}
+			}
+		}
+		*/
+		
 	}
 	
-
+	public void 
+	
 	public HiVoltsMain() {
 		init();	
 	}
@@ -49,53 +119,56 @@ public class HiVoltsMain extends Canvas implements KeyListener {
 		repaint();
 	}
 	
-	public BufferedImage imageMaker(String fileName){
-		//Name of the file in workspace
-		File image = new File(fileName);
-		String path = image.getAbsolutePath();
-		BufferedImage img = null;
-		 try {
-             img = ImageIO.read(new File(path));
-         } catch (IOException e) {
-        	 System.out.println("Oops! There was an error: " + e);
-         }
-		 return img; 
+	
+	
+	//Test Mhos
+	public void pMho(Graphics g){
+		Mho test = new Mho();
+		System.out.println(getWidth()+" "+getHeight());
+		g.drawImage(test.imageMaker(), 162, 162, getWidth()/12, getHeight()/12, null);
 	}
 	
-	public void Grid(Graphics g){
-		//Paint a grid
+	public void outerFence(Graphics g){
+		Fence outer = new Fence();
+		int lineX = getWidth()/12;
+		int lineY = getHeight()/12;
+		int xmod = 0;
+		for(int i = 1; i <= 12; i++){
+			g.drawImage(outer.imageMaker(), xmod, 0, getWidth()/12, getHeight()/12, null);
+			xmod += lineX; 
+		}
 		
-			int lineX = getWidth()/12;
-			int lineY = getHeight()/12;
-			
-			int xmod = lineX;
-			for(int i = 1; i <= 12; i++){
-				g.drawLine(xmod, 0, xmod, getHeight());
-				xmod += lineX; 
-			}
-			
-			int ymod = lineY;
-			for(int i = 1; i <= 12; i++){
-				g.drawLine(0, ymod, getWidth(), ymod);
-				ymod += lineY; 
-			}
-			
 	}
 	
+	public void paintGrid(Graphics g){
+		//Paint a grid
+		int lineX = getWidth()/12;
+		int lineY = getHeight()/12;
+		int xmod = lineX;
+		for(int i = 1; i <= 12; i++){
+			g.drawLine(xmod, 0, xmod, getHeight());
+			xmod += lineX; 
+		}
+
+		int ymod = lineY;
+		for(int i = 1; i <= 12; i++){
+			g.drawLine(0, ymod, getWidth(), ymod);
+			ymod += lineY; 
+		}
+	}
+	
+
 	int keyBla;
 	/** Paint Method
 	 * @param g - object of the graphics package
 	 */
 	public void paint(Graphics g) {
-		//Draw an image
-		g.drawImage(imageMaker("sad.png"), 500, 500, null);
-		g.clearRect(500, 500, 500, 500);
-		
-		//Paint a grid
-		Grid(g);
+		pMho(g);
+		outerFence(g);
+		paintGrid(g);
 		
 		this.addKeyListener(this);
-		//System.out.println(keyBla);
+//		System.out.println(keyBla);
 		try {
 			Thread.sleep(100);
 		} catch (InterruptedException e) {
