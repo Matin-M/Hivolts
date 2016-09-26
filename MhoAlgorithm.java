a64de8450f8269dc958b0b6ff13cfc43972770b8
Matin Massoudi <matin@massoudi.net>
2016-09-25 20:56:34 -0700

    New mhoAlgorithm class. Will go over implementation

--- MhoAlgorithm.java
+++ MhoAlgorithm.java
@@ -1,9 +1,88 @@
 
 public class MhoAlgorithm {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//These are experimental values
+		int PX;
+		int PY;
+
+		int mhoX;
+		int mhoY;
+
+		int fenceX;
+		int fenceY;
+
+
+
+	public static void MhoAlgorithm(String[] args) {
+		// This is the algorithm for the mho objects.
+
+		if(PX == mhoX){
+			if(PY > mhoY){
+				mhoY++;
+			}else{
+				mhoY--;
+			}
+
+			if((fenceY == mhoY) && (fenceX == mhoX){
+				<Remove the mho>
+			}
+
+			<collision statements here(Via mhos)>
+
+		}else if(playerY == mhoY){
+			if(PX > mhoX){
+				mhoX++;
+			}else{
+				mhoX--;
+			}
+
+			if((fenceY == mhoY) && (fenceX == mhoX){
+				<Remove the mho>
+			}
+
+			<collision statements here(Via mho)>
+
+		}else{
+			int xmod= mhoX - PX;
+			int ymod = mhoY - PY; 
+
+			if(xmod == ymod){
+				if( (xmod != Math.abs(xmod)) && (ymod != Math.abs(ymod)) ){
+					mhoX++;
+					mhoY++;
+				}else if( (xmod == Math.abs(xmod)) && (ymod != Math.abs(ymod)) ){
+					mhoX--;
+					mhoY++;
+				}else if( (xmod == Math.abs(xmod)) && (ymod == Math.abs(ymod)) ){
+					mhoX--;
+					mhoY--;
+				}else if( (xmod != Math.abs(xmod)) && (ymod == Math.abs(ymod)) ){
+					mhoX++;
+					mhoY--;
+				}
+
+
+			}else if(PX >= mhoY){
+				if(PX > mhoX){
+					mhoX++;
+				}else{
+					mhoX--;
+				}
+			}else if(PX <= mhoY){
+				if(PY > mhoY){
+					mhoY++;
+				}else{
+					mhoY--;
+			}
+			}
+
+			if((fenceY == mhoY) && (fenceX == mhoX){
+				<Remove the mho>
+			}
+
+
+		}
 
 	}
 
-}
+
