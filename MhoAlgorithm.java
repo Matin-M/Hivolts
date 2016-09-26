92ed7085c42a7047b49edcb21550a7385c714fa1
Matin Massoudi <matin@massoudi.net>
2016-09-26 11:40:18 -0700

    Update MhoAlgorithm.java

--- MhoAlgorithm.java
+++ MhoAlgorithm.java
@@ -86,3 +86,88 @@ public class MhoAlgorithm {
 	}
 
 
+			   
+			   
+			   
+			   ////////////
+			   
+			   
+
+			   public static void MhoAlgorithm() {
+			// This is the algorithm for the mho objects.
+
+			if(PX == mhoX){
+				if(PY > mhoY){
+					mhoY++;
+				}else{
+					mhoY--;
+				}
+
+				if((fenceY == mhoY) && (fenceX == mhoX){
+					<Remove the mho>
+				}
+
+				<collision statements here(Via mhos)>
+
+			}else if(playerY == mhoY){
+				if(PX > mhoX){
+					mhoX++;
+				}else{
+					mhoX--;
+				}
+
+				if((fenceY == mhoY) && (fenceX == mhoX){
+					<Remove the mho>
+				}
+
+				<collision statements here(Via mho)>
+
+			}else{
+				int xmod= mhoX - PX;
+				int ymod = mhoY - PY; 
+
+				if(xmod == ymod){
+					if( (xmod != Math.abs(xmod)) && (ymod != Math.abs(ymod)) ){
+						mhoX++;
+						mhoY++;
+					}else if( (xmod == Math.abs(xmod)) && (ymod != Math.abs(ymod)) ){
+						mhoX--;
+						mhoY++;
+					}else if( (xmod == Math.abs(xmod)) && (ymod == Math.abs(ymod)) ){
+						mhoX--;
+						mhoY--;
+					}else if( (xmod != Math.abs(xmod)) && (ymod == Math.abs(ymod)) ){
+						mhoX++;
+						mhoY--;
+					}
+
+
+				}else if(PX >= mhoY){
+					if(PX > mhoX){
+						mhoX++;
+					}else{
+						mhoX--;
+					}
+				}else if(PX <= mhoY){
+					if(PY > mhoY){
+						mhoY++;
+					}else{
+						mhoY--;
+				}
+				}
+
+				if((fenceY == mhoY) && (fenceX == mhoX){
+					<Remove the mho>
+				}
+
+
+			}
+	
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
